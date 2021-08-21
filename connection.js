8cd6ed70ccfe7e784c8e853bcb4ff8a014c4503f
K.S.Sabari Nathan <68042285+sabarinathan145@users.noreply.github.com>
2021-08-21 21:41:41 +0530

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+
+const connectTODB = async()=> mongoose.connect('mongodb+srv://sabarinathanks:ramyasabari33@userapplication.uv3in.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' , {
+    useNewUrlParser : true,
+      useFindAndModify : false,
+      useUnifiedTopology :true,
+      useCreateIndex: true
+  });   
+
+  module.exports = connectTODB;
