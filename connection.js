34f498990f90b0f124129b164fd39b3f6259677d
K.S.Sabari Nathan <68042285+sabarinathan145@users.noreply.github.com>
2021-11-28 00:53:00 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const connectTODB = async()=> mongoose.connect('mongodb+srv://sabarinathanks:ramyasabari33@userapplication.uv3in.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' , {
+const connectTODB = async()=> mongoose.connect('mongodb+srv://sabarinathanks:priyasabari33@userapplication.uv3in.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' , {
     useNewUrlParser : true,
       useFindAndModify : false,
       useUnifiedTopology :true,
